ab567b785e9a87192b8c2508b6cee47164903927
Leonardo Alves <leoalvesjf@gmail.com>
2026-03-11 09:34:48 -0300

    fix: corrige SUPABASE_KEY para JWT valido (anon key)

--- js/constants.js
+++ js/constants.js
@@ -3,6 +3,6 @@ const VERSION_URL = 'https://mvp-assistente.vercel.app/version.json';
 const UPDATE_URL = 'https://mvp-assistente.vercel.app/index.html';
 
 const SUPABASE_URL = 'https://cxhjypywqxxxhvgdvfdo.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_4M6CTKtDO-hJA4nNRt8akg_vD9quY1R';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN4aGp5cHl3cXh4eGh2Z2R2ZmRvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI3NTgyNjYsImV4cCI6MjA4ODMzNDI2Nn0.IWYSMURW3PGax5TiW7Zl2PdwJQsY0nKkUkqmxumAejQ';
 
 const isNative = () => !!(window.Capacitor && window.Capacitor.isNativePlatform && window.Capacitor.isNativePlatform());
